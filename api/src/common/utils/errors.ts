17b239bc5a93ca31b947e5b155dfcc851e78aea3
FaisalAl-Tameemi <faisal.al.tameemi@gmail.com>
2024-08-13 16:34:49 +0400

    Extend error codes

--- api/src/common/utils/errors.ts
+++ api/src/common/utils/errors.ts
@@ -6,23 +6,25 @@ export enum CustomErrorCode {
   INVALID_CREDENTIALS = 103,
   INVALID_REQUEST_DATA = 104,
   INVALID_TOKEN = 105,
+  INVALID_COMMUNITY = 106,
+  INVALID_ACCESS_CONTROL = 107,
 }
 
 export class CustomError extends Error {
-  message: string;
-  data?: unknown;
-  statusCode?: number;
-  code: CustomErrorCode;
+  message: string
+  data?: unknown
+  statusCode?: number
+  code: CustomErrorCode
 
   constructor(message: string, code: CustomErrorCode, data: unknown = {}, statusCode = 500) {
-    super(message);
-    this.message = message;
-    this.code = code;
-    this.data = data;
-    this.statusCode = statusCode;
+    super(message)
+    this.message = message
+    this.code = code
+    this.data = data
+    this.statusCode = statusCode
   }
 
   static unknown(err: Error, data: unknown = {}): CustomError {
-    return new CustomError(err.message, CustomErrorCode.UNKNOWN, data, 500);
+    return new CustomError(err.message, CustomErrorCode.UNKNOWN, data, 500)
   }
 }
